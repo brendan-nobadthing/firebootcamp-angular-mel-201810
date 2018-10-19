d27caa0f093b4fb855abf39324180a2554d8e25c
Brendan Richards <brendan_richards_msdn@hotmail.com>
2018-10-19 11:15:30 +1100

    added edit component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'company/list', pathMatch: 'full' },
-  { path: 'company/list', component: CompanyListComponent }
+  { path: 'company/list', component: CompanyListComponent },
+  { path: 'company/edit/:id', component: CompanyEditComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FormsModule } from '@angular/forms';
 import { CompanyListComponent } from './company/company-list/company-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
-import { CompanyTableComponent } from './company/company-table/company-table.component'
+import { CompanyTableComponent } from './company/company-table/company-table.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyListComponent,
-    CompanyTableComponent
+    CompanyTableComponent,
+    CompanyEditComponent
   ],
   imports: [
     BrowserModule,
